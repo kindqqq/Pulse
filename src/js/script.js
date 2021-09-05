f1a0578c4d767302ad127f4de270bf57acac0528
Alex <Markmorozov@gmail.com>
2021-09-05 19:12:36 +0300

    adaptation .section3

---
+++ src/js/script.js
@@ -0,0 +1,72 @@
+var name = "Alex";
+
+let number = 7;
+const pi = 3.14;
+
+number = 5;
+
+let leftBorderWidth = 200;
+
+
+//*  number;
+//*  string - "", '', ``;
+//*  true/false - булево значение
+//*  null - несуществующий
+//*  undefined - неопределенный
+let obj = {
+  name: 'apple',
+  color: 'green',
+  weight: 200,
+}
+//* Symbol
+
+// alert(1234);
+// console.log(number);
+
+// let answer = confirm('Вам есть 18?');
+// console.log(answer);
+
+// let answer = prompt('Вам есть 18?', "");
+// console.log(answer);
+
+// console.log(4 + "fdsg")
+
+// let isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked && isClose) /* и */
+
+// console.log(isChecked || isClose) /* или или */
+
+/* if (2 * 4 == 8 * 0) {
+  console.log("Верно")
+} else {
+  console.log('Ошибка')
+} */
+
+/* let answer = confirm('Вам есть 18?', "");
+if (answer) {
+  console.log('Come in')
+} else {
+  console.log('no entry')
+} */
+
+/* const num = 100;
+if (num < 49) {
+  console.log('Неправиьно')
+} else if (num > 100) {
+  console.log('Много')
+} else {
+  console.log("Верно")
+} */
+
+/* for (let i = 1; i < 8; i++) {
+  console.log(i)
+} */
+
+// function logging(a, b) {
+//   console.log(a + b)
+// }
+
+// logging(3, 10);
+// logging(10, 12)
